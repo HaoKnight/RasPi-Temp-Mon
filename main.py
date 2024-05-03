c416615733549f770fed688bcf51ebddcbd3fc60
HaoKnight <142246477+HaoKnight@users.noreply.github.com>
2024-05-03 12:16:21 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from temperature_window import TemperatureWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TemperatureWindow()
-    window.show()
-    sys.exit(app.exec())
